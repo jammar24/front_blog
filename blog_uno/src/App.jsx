3158badf8b607757a96d4cc28729576228e306d4
jammar24 <jamarodalis@yahoo.com>
2023-04-18 10:13:13 -0500

    commit actulizacion

--- blog_uno/src/App.jsx
+++ blog_uno/src/App.jsx
@@ -7,9 +7,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="Ap">
         hola mundo
         <h2>empecemos </h2>
+
+
+        <article>
+          <h5>estoy actualizando el sistema <span> <img src="image.png" alt="" /></span></h5>
+        </article>
     </div>
   )
 }
