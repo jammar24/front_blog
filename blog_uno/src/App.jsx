fd4aeb900617adac736ac15a46551a5ea8cd7861
jammar24 <jamarodalis@yahoo.com>
2023-04-18 10:08:40 -0500

    commit four

--- blog_uno/src/App.jsx
+++ blog_uno/src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div className="App">
+    <div className="App ">
         hola mundo
-        <h2>Hola jam desde mi rama Cristian</h2>
+        <h2>Hola jam desde mnfhnjhfjfjkfjkffjlkfjfgjñllf</h2>
         <h5>HOLA JAMAAAARRRR</h5>
+        
     </div>
   )
 }
